bae66b240c74edb9c75ff4ba573729719a77eae4
RM <ysnbtkgw@gmail.com>
2023-08-30 20:22:06 +0900

    header edited

--- src/components/PageHeader.vue
+++ src/components/PageHeader.vue
@@ -5,15 +5,14 @@
 <script>
 </script>
 <style scoped>
+
 h1 {
   box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
-  margin: 3rem auto;
-  border-radius: 10px;
   padding: 1rem;
   background-color: #0070e8;
   color: white;
   text-align: center;
   width: 100%;
-  max-width: 80rem;
+  box-sizing: border-box; 
 }
 </style>
